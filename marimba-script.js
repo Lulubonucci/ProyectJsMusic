6059e6e6b438d680c0cb4d9f26c4a10a675d490d
VictoriaCValdez <victoriacvaldez02@gmail.com>
2023-12-11 15:41:22 +0100

    desarrollando marimba

--- marimba-script.js
+++ marimba-script.js
@@ -1,13 +1,66 @@
-const teclas=document.querySelectorAll(".teclas")
+// let teclaC = document.getElementById("c")
+// function reproducirTeclaC() {
+//     let audio = new Audio
+//     audio.src = "sonidos-marimba/c.wav"
+//     audio.play()
+// }
+// teclaC.addEventListener("click", () => reproducirTeclaC())
+// document.addEventListener("keydown", (event) => {
+//     if (event.key === "a") {
+//         reproducirTeclaC()
+//     }
+// })
+
+// let teclaD = document.getElementById("d")
+// function reproducirTeclaD() {
+//     let audio = new Audio
+//     audio.src = "sonidos-marimba/b.wav"
+//     audio.play()
+// }
+// teclaD.addEventListener("click", () => reproducirTeclaD())
+// document.addEventListener("keydown", (event) => {
+//     if (event.key === "s") {
+//         reproducirTeclaD()
+//     }
+// })
+// let teclaE = document.getElementById("e")
+// function reproducirTeclaE() {
+//     let audio = new Audio
+//     audio.src = "sonidos-marimba/c.wav"
+//     audio.play()
+// }
+// teclaE.addEventListener("click", () => reproducirTeclaE())
+// document.addEventListener("keydown", (event) => {
+//     if (event.key === "d") {
+//         reproducirTeclaE()
+//     }
+// })
+// let teclaF = document.getElementById("f")
+// function reproducirTeclaF() {
+//     let audio = new Audio
+//     audio.src = "sonidos-marimba/d.wav"
+//     audio.play()
+// }
+// teclaF.addEventListener("click", () => reproducirTeclaF())
+// document.addEventListener("keydown", (event) => {
+//     if (event.key === "f") {
+//         reproducirTeclaF()
+//     }
+// })
+// let teclaG = document.getElementById("g")
+// function reproducirTeclaG() {
+//     let audio = new Audio
+//     audio.src = "sonidos-marimba/e.wav"
+//     audio.play()
+// }
+// teclaG.addEventListener("click", () => reproducirTeclaG())
+// document.addEventListener("keydown", (event) => {
+//     if (event.key === "g") {
+//         reproducirTeclaG()
+//     }
+// })
+
+let teclas=document.querySelectorAll(".teclas")
 console.log(teclas)
-function reproducirSonido(a){
-    let audio=new Audio
-    audio.src=`sonidos-marimba/${a}.wav`
-    audio.play()
-
-}
-
-teclas.forEach(tecla=>{
-console.log(tecla)
-reproducirSonido(tecla)
-})
+teclas.forEach((tecla) => let audio=new Audio
+audio.src = "sonidos-marimba/a.wav");
