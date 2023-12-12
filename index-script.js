423312e6ad23353e892629b16e947dbec35bfc0a
2007riot <2007riot@gmail.com>
2023-12-12 16:04:55 +0100

    drum id renamed

--- index-script.js
+++ index-script.js
@@ -1,7 +1,7 @@
 const instrumentButtons = document.querySelectorAll(".instrument")
 console.log(instrumentButtons)
 function openInstrumnet(name) {
-    window.location.href = `${name}.html` || "marimba.html"
+    window.location.href = `${name}.html`
 }
 instrumentButtons.forEach(i => {
     i.addEventListener("click", () => openInstrumnet(i.id))
