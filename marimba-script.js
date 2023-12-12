e081239ad313b2ee86ac523889371b9b21cb6822
2007riot <2007riot@gmail.com>
2023-12-12 10:06:31 +0100

    Marimba

--- marimba-script.js
+++ marimba-script.js
@@ -1,13 +1,15 @@
 const teclas=document.querySelectorAll(".teclas")
-console.log(teclas)
-function reproducirSonido(a){
+// console.log(teclas)
+function reproducirSonido(tecla){
     let audio=new Audio
-    audio.src=`sonidos-marimba/${a}.wav`
+    audio.src=`sonidos-marimba/${tecla}.wav`
     audio.play()
-
+    const clickedKey = document.getElementById(tecla)
+    console.log(clickedKey)
 }
 
 teclas.forEach(tecla=>{
-console.log(tecla)
-reproducirSonido(tecla)
+tecla.addEventListener("click", ()=>reproducirSonido(tecla.id))
+console.log("tecla " + tecla.id)
 })
+// document.addEventListener("keydown")
